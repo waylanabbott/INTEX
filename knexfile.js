04765f92a870b057c683d18c776d516346cef167
sammjen <samjen@byu.edu>
2025-12-01 20:41:47 -0700

    i think i fixed the connection (for real)

--- knexfile.js
+++ knexfile.js
@@ -18,8 +18,12 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
+      host: process.env.RDS_HOSTNAME || 'awseb-e-2zsmnexgnv-stack-awsebrdsdatabase-wguhpqqhmbo1.cpyq0oiq43rn.us-east-2.rds.amazonaws.com',
+      port: process.env.RDS_PORT || 5432,
+      user: process.env.RDS_USERNAME || 'group25',
+      password: process.env.RDS_PASSWORD || 'MysteriousTurtle1!',
+      database: process.env.RDS_DB_NAME || 'ebdb',
+      ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false 
     },
     pool: { min: 2, max: 10 },
     migrations: { directory: './migrations' }
