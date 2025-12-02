6536ee469c079841b645232192bf1d95a219dcd8
sammjen <samjen@byu.edu>
2025-12-02 11:25:04 -0700

    fixing certs?

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # .platform/hooks/postdeploy/00_get_certificate.sh
-sudo certbot -n -d http://ellarises-2-5.is404.net --nginx --agree-tos --email samjen@byu.edu
+sudo certbot -n -d ellarises-2-5.is404.net --nginx --agree-tos --email samjen@byu.edu
